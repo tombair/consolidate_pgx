7522c528029ca615f35dfb11665bd7b68fc5bf91
tombair <tom.bair@gmail.com>
2016-07-13 14:58:36 -0500

    last change before txt mining

--- consolidate_pgx.py
+++ consolidate_pgx.py
@@ -3,88 +3,120 @@
 import openpyxl
 import csv
 import sys
+import re
 
 wb = openpyxl.load_workbook(filename=sys.argv[1])
 
 sheets = wb.get_sheet_names()
 
-ws =wb.get_sheet_by_name('Med Admin for Adverse Visits')
-data = {}
 phenodata = {}
-adverse_meds = {}
-all_meds = {}
+meds = {}
+def mednametruc(name):
+    mapping = {'.':'', ',':'','\t':'','\n':'',' ':'', 'mg':'','tablet':'','oral':'','MG':'','patch':'', 'ML':'', 'mL':'', 'powder':'','capsule':'', 'IV':'', 'drops':''}
+    for k,v in mapping.iteritems():
+        name = name.replace(k,v)
+    name = re.sub('\d', '', name)
+    return name
 
-
-allmedsdl = []
+    
+drug_list = [] #a list of all possible drugs prepend Adverse_ to the front of drugs used in response to adverse events
+ws =wb.get_sheet_by_name('Med Admin for Adverse Visits')
 for r in ws.iter_rows():
-    allmedsdl.append(r[8].value)
-allmedsdl = list(set(allmedsdl))
+    medName = 'Adverse_'+r[8].value
+    medName = mednametruc(medName)
+    drug_list.append(medName)
+ws = wb.get_sheet_by_name('All Rx Medications')
+for r in ws.iter_rows():
+    medName = r[4].value
+    if medName is not None:
+        medName = mednametruc(medName)
+        drug_list.append(medName)
+ws = wb.get_sheet_by_name('All Pt Reported Medications')
+for r in ws.iter_rows():
+    medName = r[4].value
+    if medName is not None:
+        medName = mednametruc(medName)
+        drug_list.append(medName)
+drug_list = list(set(drug_list))
+meds['ID'] = drug_list
+
+ws =wb.get_sheet_by_name('Med Admin for Adverse Visits')
 for r in ws.iter_rows():
     ptId = r[0].value
-    oneMonth = r[2].value
-    medID =  r[7].value
-    medName = r[8].value
-    if not data.has_key(ptId):
-        data[ptId] = {}
-    if not data[ptId].has_key(medID):
-        data[ptId][medID]={'Name':medName,'count':0,'oneMonth':oneMonth}
-    data[ptId][medID]['count'] += 1
-    if not adverse_meds.has_key(r[0].value):
-        adverse_meds[r[0].value] = [0]*len(allmedsdl)
-    index_pos = allmedsdl.index(r[8].value)
-    adverse_meds[r[0].value][index_pos] = 1
+    medName = 'Adverse_'+r[8].value
+    medName = mednametruc(medName)
+    if not meds.has_key(ptId):
+        meds[ptId] = [0]*len(drug_list)
+    index_pos = drug_list.index(medName)
+    meds[ptId][index_pos] = 1
+ws = wb.get_sheet_by_name('All Rx Medications')
+for r in ws.iter_rows():
+    ptId = r[0].value
+    medName = r[4].value
+    if medName is not None:
+        medName = mednametruc(medName)
+        if not meds.has_key(ptId):
+            meds[ptId] = [0]*len(drug_list)
+        index_pos = drug_list.index(medName)
+        meds[ptId][index_pos] = 1
+ws = wb.get_sheet_by_name('All Pt Reported Medications')
+for r in ws.iter_rows():
+    ptId = r[0].value
+    medName = r[4].value
+    if medName is not None:
+        medName = mednametruc(medName)
+        if not meds.has_key(ptId):
+            meds[ptId] = [0]*len(drug_list)
+        index_pos = drug_list.index(medName)
+        meds[ptId][index_pos] = 1
+#now should have a drug use matrix indexed by ptID full of zeros (mostly) and ones
 
 ws = wb.get_sheet_by_name('Patient Visits')
 #go through and get pt id and total up the ammount spent (I) and LOS (E)
 for r in ws.iter_rows():
     if not phenodata.has_key(r[0].value):
-        phenodata[r[0].value] = {}
+        phenodata[r[0].value] = {"Total_Charges":'NA',"LOS":'NA',"DX_Name":'NA',"Adverse_ICD9":'NA'}
     phenodata[r[0].value]['Total_Charges'] = r[8].value
     phenodata[r[0].value]['LOS'] = r[4].value
 
 ws = wb.get_sheet_by_name('DX')
 # go through and associate ptid with DX_NAME(D) and E and F and G
 for r in ws.iter_rows():
-    if not phenodata.has_key(r[0].value):
-        phenodata[r[0].value] = {}
-    phenodata[r[0].value]['DX_Name'] = r[3].value
-    phenodata[r[0].value]['Adverse_ICD9'] = r[6].value
+    if phenodata.has_key(r[0].value):
+        phenodata[r[0].value]['DX_Name'] = r[3].value.replace(',',' ')
+        phenodata[r[0].value]['Adverse_ICD9'] = r[6].value.replace(',',' ')
 
-
-#get a master list of meds
-drug_list = []
-ws = wb.get_sheet_by_name('All Rx Medications')
-for r in ws.iter_rows():
-    drug_list.append(r[4].value)
-ws = wb.get_sheet_by_name('All Pt Reported  Medications')
-for r in ws.iter_rows():
-    drug_list.append(r[4].value)
-drug_list = list(set(drug_list))
-drug_matrix = {"ID":drug_list}
-
-ws = wb.get_sheet_by_name('All Rx Medications')
-for r in ws.iter_rows():
-    if not drug_matrix.has_key(r[0].value):
-        drug_matrix[r[0].value] = [0]*len(drug_list)
-    index_pos = drug_list.index(r[4].value)
-    drug_matrix[r[0].value][index_pos] = 1
-ws = wb.get_sheet_by_name('All Pt Reported  Medications')
-for r in ws.iter_rows():
-    if not drug_matrix.has_key(r[0].value):
-        drug_matrix[r[0].value] = [0]*len(drug_list)
-    index_pos = drug_list.index(r[4].value)
-    drug_matrix[r[0].value][index_pos] = 1
-
-
-header = ("Total_Charges","LOS","DX_Name","Adverse_ICD9")
-allheader = header + adverse_meds['ID']
-print ",".join(allheader)
-for x in adverse_meds:
-    if not x == 'ID':
-        prefix = []
+#need to make sure phenodata only has ptids that are in meds
+newpheno = {}
+for x in phenodata:
+    if meds.has_key(x):
+        newpheno[x] = phenodata[x]
+with open("phenodata.csv", 'w') as pheno_out:
+    header = ["Total_Charges","LOS","DX_Name","Adverse_ICD9"]
+    pheno_out.write("ID,"+",".join(header))
+    pheno_out.write("\n")
+    for x in newpheno:
+        out = [x]
         for h in header:
-            prefix.append(phenodata[h])
-        print ",".join(prefix+adverse_meds[x])
+            try:
+                out.append(newpheno[x][h])
+            except:
+                pass
+        pheno_out.write(",".join(map(str,out)))
+        pheno_out.write("\n")
+with open("drug_data.csv",'w') as drug_out:
+    drug_out.write("ID\t"+"\t".join(map(str,meds['ID'])))
+    drug_out.write("\n")
+    for id in newpheno:
+        try:
+            drug_out.write(id+"\t"+"\t".join(map(str,meds[id])))
+            drug_out.write("\n")
+        except:
+            pass #in meds but not newpheno
+        
+
+
+
 
         
     
--- pdx_analysis.R
+++ pdx_analysis.R
@@ -1,9 +1,13 @@
-d <- read.csv(file="phenodata.csv",sep=",",header=TRUE,stringsAsFactors = FALSE)
+library("plyr")
+
+d <- read.csv(file="phenodata.csv",sep=",",header=TRUE,stringsAsFactors = FALSE,row.names=1)
 d$Adverse_ICD9_simple <- gsub ("causing adverse effect in therapeutic use","",d$Adverse_ICD9)
 d$Adverse_ICD9_simple <- strtrim(d$Adverse_ICD9_simple,50)
+d$DX_Name_simple <- gsub ("causing adverse effect in therapeutic use","",d$DX_Name)
+d$DX_Name_simple <- strtrim(d$DX_Name_simple,50)
 
 d10 <- d[ d$Adverse_ICD9 %in% names(table(d$Adverse_ICD9))[table(d$Adverse_ICD9) >10],]
-
+dx10 <- d[ d$DX_Name %in% names(table(d$DX_Name))[table(d$DX_Name) >10],]
 png("adverse_hist.png",width = 8.5, height=11, units="in",res=150)
 par(mar=c(20,8,8,2))
 plot(sort(decreasing = TRUE,table(d$Adverse_ICD9_simple))[1:30],las=3,yaxt="n",ylab="")
@@ -19,6 +23,12 @@ title(main="Adverse ICD9 with > 10 events", ylab="Total Charges", line=5)
 axis(2,las=2)
 dev.off()
 
+png("dx_boxplot_cost.png",width = 8.5, height=11, units="in",res=150)
+par(mar=c(20,8,8,2))
+boxplot(as.numeric(Total_Charges)~DX_Name_simple,data=dx10,yaxt="n",las=3)
+title(main="Adverse ICD9 with > 10 events", ylab="Total Charges", line=5)
+axis(2,las=2)
+dev.off()
 png("adverse_boxplot_los.png",width = 8.5, height=11, units="in",res=150)
 par(mar=c(20,8,8,2))
 boxplot(as.numeric(LOS)~Adverse_ICD9_simple,data=d10,yaxt="n",las=3)
@@ -26,3 +36,14 @@ title(main="Adverse ICD9 with > 10 events", ylab="Length of Stay", line=5)
 axis(2,las=2)
 dev.off()
 
+drug <- read.csv(file="drug_data.csv",sep=",",header=TRUE,stringsAsFactors = FALSE,row.names = 1)
+drug20 <- drug[,(colSums(drug)) >20]
+d<- ddply(d,'Adverse_ICD9', transform, median = median(Total_Charges))
+d$expensive <- ifelse(d$Total_Charges>d$median, 'YES', 'NO')
+
+#subset to Antineoplastic and immunosuppressive drugs
+drugAIds <- subset(d, d$Adverse_ICD9_simple == "Antineoplastic and immunosuppressive drugs ")
+drugsub <- drug20[rownames(drug20) %in% rownames(drugAIds),]
+drugsub <- drugsub[,apply(drugsub, 2, var, na.rm=TRUE) >= .25]
+drug.pca <- prcomp(drugsub, scale = FALSE)
+ggbiplot(drug.pca)
